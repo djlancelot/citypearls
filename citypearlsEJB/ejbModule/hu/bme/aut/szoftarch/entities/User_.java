8c3790ae4f399b59888f82854d2d1b366da42801
László Velinszky <velinszkylaci@gmail.com>
2013-11-12 23:36:35 +0100

    Finally removed j2ee utility project. Git repo works like charm.

--- citypearlsEJB/ejbModule/hu/bme/aut/szoftarch/entities/User_.java
+++ citypearlsEJB/ejbModule/hu/bme/aut/szoftarch/entities/User_.java
@@ -5,7 +5,7 @@ import javax.persistence.metamodel.SetAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="Dali", date="2013-11-12T09:35:26.232+0100")
+@Generated(value="Dali", date="2013-11-12T23:35:06.694+0100")
 @StaticMetamodel(User.class)
 public class User_ {
 	public static volatile SingularAttribute<User, Integer> id;
@@ -13,7 +13,7 @@ public class User_ {
 	public static volatile SingularAttribute<User, Integer> groupid;
 	public static volatile SingularAttribute<User, String> password;
 	public static volatile SingularAttribute<User, String> username;
+	public static volatile SingularAttribute<User, Question> lastQuestion;
 	public static volatile SetAttribute<User, Answer> answers;
 	public static volatile SetAttribute<User, Question> questions;
-	public static volatile SingularAttribute<User, Question> lastQuestion;
 }
