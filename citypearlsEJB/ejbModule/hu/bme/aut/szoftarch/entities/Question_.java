7e30ad60a32ed190fcfebf89f0e5510a792432e1
László Velinszky <velinszkylaci@gmail.com>
2013-11-20 00:35:40 +0100

    Mit classes

--- citypearlsEJB/ejbModule/hu/bme/aut/szoftarch/entities/Question_.java
+++ citypearlsEJB/ejbModule/hu/bme/aut/szoftarch/entities/Question_.java
@@ -4,13 +4,13 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="Dali", date="2013-10-12T11:40:16.419+0200")
+@Generated(value="Dali", date="2013-11-20T00:34:17.498+0100")
 @StaticMetamodel(Question.class)
 public class Question_ {
 	public static volatile SingularAttribute<Question, Integer> id;
+	public static volatile SingularAttribute<Question, String> address;
 	public static volatile SingularAttribute<Question, String> answer;
 	public static volatile SingularAttribute<Question, String> description;
-	public static volatile SingularAttribute<Question, String> address;
 	public static volatile SingularAttribute<Question, Float> latitude;
 	public static volatile SingularAttribute<Question, Float> longtitude;
 	public static volatile SingularAttribute<Question, Integer> point;
--- citypearlsEJB/ejbModule/hu/bme/aut/szoftarch/entities/User_.java
+++ citypearlsEJB/ejbModule/hu/bme/aut/szoftarch/entities/User_.java
@@ -4,13 +4,13 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="Dali", date="2013-10-12T11:40:16.503+0200")
+@Generated(value="Dali", date="2013-11-20T00:34:17.512+0100")
 @StaticMetamodel(User.class)
 public class User_ {
 	public static volatile SingularAttribute<User, Integer> id;
+	public static volatile SingularAttribute<User, String> email;
 	public static volatile SingularAttribute<User, Integer> groupid;
 	public static volatile SingularAttribute<User, String> password;
-	public static volatile SingularAttribute<User, String> email;
-	public static volatile SingularAttribute<User, Integer> score;
 	public static volatile SingularAttribute<User, String> username;
+	public static volatile SingularAttribute<User, Integer> score;
 }
